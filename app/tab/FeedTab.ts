a37f0dd69f29c993a2da69d438d846b1054b7dec
yjgaia <yjdev88@gmail.com>
2024-05-20 18:26:28 +0900

    dev

--- app/tab/FeedTab.ts
+++ app/tab/FeedTab.ts
@@ -1,14 +1,30 @@
-import { Activatable, el, Tabs } from "@common-module/app";
 import {
+  Activatable,
+  Button,
+  ButtonType,
+  el,
+  InfoMessageBox,
+  msg,
+  Store,
+  Supabase,
+  Tabs,
+} from "@common-module/app";
+import {
+  openUserOrCreatorModal,
   Post,
   PostForm,
   PostListFollowing,
   PostListForYou,
   PostSingle,
+  SFSignedUserManager,
+  SFUserService,
+  UserAvatar,
 } from "fsesf";
 import TitleBarUserButton from "../component/TitleBarUserButton.js";
 
 export default class FeedTab extends Activatable {
+  private referrelStore = new Store("referral");
+
   private tabs: Tabs;
 
   private postForm: PostForm;
@@ -38,10 +54,10 @@ export default class FeedTab extends Activatable {
 
     this.postForm.on("post", (post: Post) => {
       this.postListForYou.prepend(
-        new PostSingle(post, { hasChild: false, main: false }),
+        new PostSingle(post, { hasChild: false, main: false, full: false }),
       );
       this.postListFollowing.prepend(
-        new PostSingle(post, { hasChild: false, main: false }),
+        new PostSingle(post, { hasChild: false, main: false, full: false }),
       );
     });
 
@@ -50,5 +66,60 @@ export default class FeedTab extends Activatable {
       if (id === "for-you") this.postListForYou.show();
       else if (id === "following") this.postListFollowing.show();
     }).init();
+
+    if (this.referrelStore.get("from")) {
+      if (!SFSignedUserManager.signed) {
+        this.loadReferrer();
+      } else {
+        Supabase.client.functions.invoke(
+          "use-referrel-code",
+          { body: { referrer: this.referrelStore.get("from") } },
+        );
+      }
+    }
+  }
+
+  private async loadReferrer() {
+    const from = this.referrelStore.get<string>("from");
+    if (from) {
+      const user = await SFUserService.fetchByXUsername(from);
+      if (user) {
+        el(
+          ".referral-info-message-container",
+          new InfoMessageBox({
+            message: [
+              "You have been invited by ",
+              el(
+                "a.user",
+                new UserAvatar(user.user_id, [
+                  user.avatar_thumb,
+                  user.stored_avatar_thumb,
+                ]),
+                el("span.name", user.display_name),
+                { click: () => openUserOrCreatorModal(user) },
+              ),
+              ". If you login, ",
+              el(
+                "a.user",
+                new UserAvatar(user.user_id, [
+                  user.avatar_thumb,
+                  user.stored_avatar_thumb,
+                ]),
+                el("span.name", user.display_name),
+                { click: () => openUserOrCreatorModal(user) },
+              ),
+              " will earn ",
+              el("b", "10 points"),
+              ".",
+            ],
+            footer: new Button({
+              type: ButtonType.Contained,
+              title: msg("login-required-login-button"),
+              click: () => SFSignedUserManager.signIn(),
+            }),
+          }),
+        ).appendTo(this, 1);
+      }
+    }
   }
 }
