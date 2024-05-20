39f90aea753e6a63e02eb2bea7c84d1f51ef66a1
yjgaia <yjdev88@gmail.com>
2024-05-20 13:19:45 +0900

    Dev notification

--- app/tab/NotificationsTab.ts
+++ app/tab/NotificationsTab.ts
@@ -1,4 +1,5 @@
 import { Activatable, el } from "@common-module/app";
+import { NotificationList } from "fsesf";
 import TitleBarUserButton from "../component/TitleBarUserButton.js";
 
 export default class NotificationsTab extends Activatable {
@@ -10,7 +11,7 @@ export default class NotificationsTab extends Activatable {
         el(".left", new TitleBarUserButton()),
         el("h1", "Notifications"),
       ),
-      el("main", el("p.wip", "No notifications yet. Stay tuned!")),
+      el("main", new NotificationList()),
     );
   }
 }
